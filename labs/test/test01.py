dea7fab44c45d65bf03a4de515e4d603df2898a8
gmaradiaga <gonzalo.maradiaga.gmail.com>
2022-09-08 17:06:09 -0600

    Commit del codigo fuente del 08.09.22 5.05pm

---
+++ labs/test/test01.py
@@ -0,0 +1,2 @@
+fruta = input("Cuál es tú fruta preferida? ")
+print("Tu fruta preferida es: " + fruta)
---
+++ labs/test/test02.py
@@ -0,0 +1,4 @@
+cantidad = input("Cuántas celulares necesitas? ")
+valor = input("Precio de cada celular: ")
+total = int(cantidad) * float(valor)
+print("El total es: ", total)
