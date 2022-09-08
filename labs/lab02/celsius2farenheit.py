f59509e9ea22ecf93302651ae666d53cad57e3c2
gmaradiaga <gonzalo.maradiaga.gmail.com>
2022-09-08 00:21:40 -0600

    Commit Lab02 v3

---
+++ labs/lab02/celsius2farenheit.py
@@ -0,0 +1,5 @@
+# Tendencias e Innovación en Tecnología Agrícola (TEA)
+
+celsius = input("Ingrese temperatura en grados Celsius: ")
+farenheit = (float(celsius) * 9/5) + 32
+print("La temperatura en grados Farenheit es: ", farenheit)
