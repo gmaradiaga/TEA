2555ee99ad4bfb12f3a4cd9d98b80efac7917362
gmaradiaga <gonzalo.maradiaga.gmail.com>
2022-09-09 11:11:18 -0600

    Versión código fuente 09.09.2022 11.11am

---
+++ labs/prueba/prueba01.py
@@ -0,0 +1,3 @@
+nombre = input("Cuál es su nombre? ")
+apellidos = input("Cuáles son sus apellidos? ")
+print("Su nombre completo es: " + nombre + " " + apellidos)
---
+++ labs/prueba/prueba02.py
@@ -0,0 +1,4 @@
+cantidad = input("Cuántos celulares necesita? ")
+valor = input("Cuál es el precio por cada celular? ")
+total = int(cantidad) * float(valor)
+print(total)
