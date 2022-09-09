75af91e74f78e66bce0ae9ae7804310f071fc2d6
gmaradiaga <gonzalo.maradiaga.gmail.com>
2022-09-09 09:15:22 -0600

    Versión codigo fuente 09.09.2022 09.15pm

--- labs/temporal/temporal02.py
+++ labs/temporal/temporal02.py
@@ -2,3 +2,5 @@ cantidad = input("Cuántos celulares? ")
 valor = input("Costo de cada celular? ")
 total = int(cantidad) * float(valor)
 print("El costo de los celulares e: ", total)
+
+# jsdhfjksdhfkjsdhfjhsdkjfk"
