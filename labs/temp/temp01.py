653506bcbb1abd06e298633dece99eb3d9a0096a
gmaradiaga <gonzalo.maradiaga.gmail.com>
2022-09-09 15:11:22 -0600

    Version del codigo fuente 09.09.2022 3.12pm

---
+++ labs/temp/temp01.py
@@ -0,0 +1,3 @@
+nombre = input("Cúal es su nombre? ")
+apellidos = input("Cúal es su apellido? ")
+print("Su nombre es: " + nombre + " " + apellidos)
---
+++ labs/temp/temp02.py
@@ -0,0 +1,4 @@
+cantidad = input("Cuántos celulares necesita? ")
+valor = input("Cuál es el valor de cada celular? ")
+total = int(cantidad) * float(valor)
+print("El precio total es: ", total)
